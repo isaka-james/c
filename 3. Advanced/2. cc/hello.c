a047be4dddabc6dc7a224bdf947c540723ed5ad0
isaka-james <mr.isakajames@gmail.com>
2024-12-28 18:00:30 -0500

    cc in check: - playing with cc

---
+++ 3. Advanced/2. cc/hello.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+
+void print_from_another_file(){
+	printf("Hello There!");
+}
---
+++ 3. Advanced/2. cc/hello_main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void print_from_another_file();
+
+int main(void){
+	print_from_another_file();
+	return 0;
+}
+
